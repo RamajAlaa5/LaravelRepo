19828f1e107d61c7f1d72f35feb123cae0e6ecd5
RamajAlaa5 <ramojaalaa15@gmail.com>
2022-04-24 11:49:32 +0200

    Added Api/PostController, Resources/PostResource

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
 {
     public function index(){
          $posts =Post::all();
-         return  PostResource::Collection($posts);
+         return  PostResource::collection($posts);
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,14 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('/test',function(){
 return "this is a test";
 });
-Route::get("/posts",[PostController::class,'index']);
+Route::get("/posts",[PostController::class,'index'])->middleware('auth:sanctum');
 Route::post("/posts",[PostController::class,'store']);
 Route::get("/posts/{post}",[PostController::class,'show']);
+
+// Route::post('sanctum/token',function(Request $request){
+//  $request->validate([
+//       'email'=>'required|email',
+//       'password'=>'required',
+//       'device_name'=>'required'
+//  ]);
+// });
+
+// $user = User::where('email',$request->email)->first();
 
